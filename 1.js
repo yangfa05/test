59d4296ac8c2ff864d1f8b88f31cfd47d4f045e0
yangfa05 <92768789+yangfa05@users.noreply.github.com>
2021-10-18 21:52:27 -0500

    Update 1.js

--- 1.js
+++ 1.js
@@ -55,7 +55,7 @@ window.gibinit = function() {
 
   function target_addToCart(DEBUG_NODE, UPC) {
     var cartUrl = 'https://carts.target.com/web_checkouts/v1/cart_items?field_groups=CART%2CCART_ITEMS%2CSUMMARY&key=feaf228eb2777fd3eee0fd5192ae7107d6224b39';
-    var cart = {"cart_type":"REGULAR","channel_id":"10","shopping_context":"DIGITAL","cart_item":{"tcin":UPC,"quantity":2,"item_channel_id":"10"},"fulfillment":{"fulfillment_test_mode":"grocery_opu_team_member_test"}};
+    var cart = {"cart_type":"REGULAR","channel_id":"10","shopping_context":"DIGITAL","cart_item":{"tcin":UPC,"quantity":1,"item_channel_id":"10"},"fulfillment":{"fulfillment_test_mode":"grocery_opu_team_member_test"}};
     
     DEBUG_NODE.innerText = 'Attempting Cart Add...';
 
